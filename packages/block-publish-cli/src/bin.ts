cc5b553f3f21fb9e9946adff3acb71ea37bc983d
ryan.li <ryan.li@aax.com>
2021-12-26 20:41:34 +0800

    update: update config

--- packages/block-publish-cli/src/bin.ts
+++ packages/block-publish-cli/src/bin.ts
@@ -2,4 +2,13 @@
 
 import { argv } from "process";
 
-console.log("Hello sj cli! Hi!", argv);
+interface IParam {
+    a: number;
+    b: number;
+}
+
+export function sum(pram: IParam) {
+    return pram.a + pram.b;
+}
+
+console.log("Hello sj cli! Hi!", sum({ a: 1, b: 2 }), argv);
